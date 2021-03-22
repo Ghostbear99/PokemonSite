8f9338cfb810f932c4344ef47c3e2348c9c123b0
Chris Gawbill <gawbillchris@gmail.com>
2021-03-22 12:57:20 -0400

    I think it will work!

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.sendFile(__dirname + '/public/home.html');
 })
 app.get('/pokemon/:pokemonName', function(req, res){
     P.getPokemonByName(req.params.pokemonName, function(response, error) { // with callback
