11d8a2a65e41d852741b5cb2013565fa83449201
Chris Gawbill <gawbillchris@gmail.com>
2021-03-22 13:01:07 -0400

    Maybe it will work

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ var options = {
 
 const port = process.env.PORT || 8000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/home.html');
 })
