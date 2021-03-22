220915e6f80c960f1d426114cf55d684753757ff
Chris Gawbill <gawbillchris@gmail.com>
2021-03-22 12:27:09 -0400

    Added some stuff for heroku

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ var options = {
   }
   var P = new Pokedex(options);
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 app.get('/pokemon/:pokemonName', function(req, res){
